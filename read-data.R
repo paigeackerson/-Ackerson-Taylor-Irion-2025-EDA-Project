c0bbfb8f723ed750c5893ce522bb4c87e3efb834
paige ackerson <no1373gk@go.minnstate.edu>
2025-01-30 13:15:21 -0600

    Species filter

--- read-data.R
+++ read-data.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(auk)
+library(magrittr)
 
 
 # save stage commit push 
@@ -14,15 +15,28 @@ library(auk)
 # get the path to the example data included in the package
 # in practice, provide path to ebd, e.g. f_in <- "data/ebd_relFeb-2018.txt
 f_in <- "data/White Collared Manakin/ebd_BR-MS-027_whcman1_smp_relDec-2024.txt"
+
 # output text file
 f_out <- "ebd_filtered_whcman1.txt"
-ebird_data <- f_in %>% 
+ebird_data <- f_in |> 
   # 1. reference file
-  auk_ebd() %>% 
+auk_ebd() |>
+
   # 2. define filters
-  auk_species(species = "Canada Jay") %>% 
-  auk_country(country = "Canada") %>% 
+  auk_species(species = "Manacus candei") |> 
+  auk_country(country = "Costa Rica")  |>  
+
+  auk_distance(distance = c(0,2)) |> 
+
+
+  auk_protocol(protocol = c("Stationary", "Traveling")) |>
+
+
+  auk_time(start_time = c("06:00", "18:00")) |>
+  auk_complete() |>
   # 3. run filtering
-  auk_filter(file = f_out) %>% 
+  auk_filter(file = f_out) |> 
   # 4. read text file into r data frame
   read_ebd()
+
+  
