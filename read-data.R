fba1f420e6588b18fb67aac0692fc82ff52784ec
paige ackerson <no1373gk@go.minnstate.edu>
2025-02-04 14:55:12 -0600

    Fixed the empty whcman data

--- read-data.R
+++ read-data.R
@@ -14,19 +14,20 @@ library(magrittr)
 # path to the ebird data file, here a sample included in the package
 # get the path to the example data included in the package
 # in practice, provide path to ebd, e.g. f_in <- "data/ebd_relFeb-2018.txt
-f_in <- "data/White Collared Manakin/ebd_BR-MS-027_whcman1_smp_relDec-2024.txt"
+f_in <- "data/ebd_CR_whcman1_smp_relDec-2024/ebd_CR_whcman1_smp_relDec-2024.txt"
 
 # output text file
 f_out <- "ebd_filtered_whcman1.txt"
-ebird_data <- f_in |> 
+ebird_data <- 
+  f_in |> 
   # 1. reference file
-auk_ebd() |>
+  auk_ebd() |>
 
   # 2. define filters
-  auk_species(species = "Manacus candei") |> 
-  auk_country(country = "Costa Rica")  |>  
+  auk_species(species = "Manacus candei") |>
+  auk_country(country = "Costa Rica")  |>
 
-  auk_distance(distance = c(0,2)) |> 
+  auk_distance(distance = c(0,2)) |>
 
 
   auk_protocol(protocol = c("Stationary", "Traveling")) |>
@@ -35,7 +36,8 @@ auk_ebd() |>
   auk_time(start_time = c("06:00", "18:00")) |>
   auk_complete() |>
   # 3. run filtering
-  auk_filter(file = f_out) |> 
+  auk_filter(file = f_out, overwrite=TRUE) |>
+
   # 4. read text file into r data frame
   read_ebd()
 
